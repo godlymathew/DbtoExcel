5090a012157d98178878106002f3b851becf1fd3
Mr Godly Mathew <godly.mathew@bcmch.org>
2019-02-16 13:02:06 +0530

    First upload
    First upload

---
+++ ProjectDBtoExcel/FrmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.BtnStartProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(21, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(290, 524);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(330, 110);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(250, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect Database";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // BtnStartProcess
+            // 
+            this.BtnStartProcess.Location = new System.Drawing.Point(330, 139);
+            this.BtnStartProcess.Name = "BtnStartProcess";
+            this.BtnStartProcess.Size = new System.Drawing.Size(250, 23);
+            this.BtnStartProcess.TabIndex = 4;
+            this.BtnStartProcess.Text = "Start Process";
+            this.BtnStartProcess.UseVisualStyleBackColor = true;
+            this.BtnStartProcess.Click += new System.EventHandler(this.BtnStartProcess_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 570);
+            this.Controls.Add(this.BtnStartProcess);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.listBox1);
+            this.Name = "frmMain";
+            this.Text = "Db to Excel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button BtnStartProcess;
+    }
+}
+
---
+++ ProjectDBtoExcel/FrmMain.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using OfficeOpenXml;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmMain : Form
+    {
+
+        string connectionString = "Data Source={IP/Name}; Initial Catalog = {DatabaseName}}; Persist Security Info=True;User ID = sa; Password=******";
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveExcel(DataTable dataTable, string newFile)
+        {
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add(newFile);
+                ws.Cells["A1"].LoadFromDataTable(dataTable, true);
+                pck.SaveAs( new System.IO.FileInfo("d:\\Export\\" + newFile + ".xlsx"));
+            }
+        }
+
+        private DataTable GetData(string tableName)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM " + tableName, sqlCon);
+                sqlCon.Open();
+                var reader = sqlCommand.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                return dt;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void BtnStartProcess_Click(object sender, EventArgs e)
+        {
+            for (var i = 0; i < listBox1.Items.Count; i++)
+            {
+                var fn = listBox1.Items[i].ToString();
+                var dt = GetData(fn);
+                if (dt != null)
+                {
+                    SaveExcel(dt, fn);
+                }
+            }
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM sys.Tables", sqlCon);
+                sqlCon.Open();
+                var reader = sqlCommand.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        listBox1.Items.Add(reader[0].ToString());
+                    }
+                }
+            }
+        }
+    }
+}
